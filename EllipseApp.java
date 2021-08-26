8dd47965f9a4d68dc9765904719e674dc4e52ca0
ianantes <49240898+ianantes@users.noreply.github.com>
2021-08-25 22:40:45 -0300

    Update and rename RectEllipseApp.java to EllipseApp.java

--- EllipseApp.java
+++ EllipseApp.java
@@ -4,18 +4,17 @@ import javax.swing.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Ellipse2D.Double;
 
-class RectEllipseApp {
+class EllipseApp {
     public static void main (String[] args) {
-        RectEllipseFrame frame = new RectEllipseFrame();
+        EllipseFrame frame = new EllipseFrame();
         frame.setVisible(true);
     }
 }
 
-class RectEllipseFrame extends JFrame {
-    Rect r1;
+class EllipseFrame extends JFrame {
     Ellipse e1, e2, e3;
 
-    RectEllipseFrame () {
+    EllipseFrame () {
         this.addWindowListener (
             new WindowAdapter() {
                 public void windowClosing (WindowEvent e) {
@@ -23,42 +22,20 @@ class RectEllipseFrame extends JFrame {
                 }
             }
         );
-        this.setTitle("Rect + Ellipse");
+        this.setTitle("Ellipse");
         this.setSize(350, 350);
-        this.r1 = new Rect(50,50, 100,30);
         this.e1 = new Ellipse(50,100, 100,30, Color.blue, Color.black);
-	      this.e2 = new Ellipse(50,200, 100,30, Color.yellow, Color.gray);
+	this.e2 = new Ellipse(50,200, 100,30, Color.yellow, Color.gray);
         this.e3 = new Ellipse(50,300, 100,30, Color.red, Color.pink);
     }
 
     public void paint (Graphics g) {
         super.paint(g);
-        this.r1.paint(g);
         this.e1.paint(g);
         this.e2.paint(g);
-	      this.e3.paint(g);
-    }
-}
-
-class Rect {
-    int x, y;
-    int w, h;
-
-    Rect (int x, int y, int w, int h) {
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
+	this.e3.paint(g);
     }
 
-    void print () {
-        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
-            this.w, this.h, this.x, this.y);
-    }
-
-    void paint (Graphics g) {
-        g.drawRect(this.x,this.y, this.w,this.h);
-    }
 }
 
 class Ellipse {
@@ -71,8 +48,8 @@ class Ellipse {
         this.y = y;
         this.w = w;
         this.h = h;
-	      this.cf = cf;
-	      this.cc = cc;
+	this.cf = cf;
+	this.cc = cc;
     }
 
     void print () {
@@ -82,9 +59,9 @@ class Ellipse {
 
     void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-	      g2d.setColor(this.cc);
+	g2d.setColor(this.cc);
         g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
-	      g2d.setColor(this.cf);
-	      g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+	g2d.setColor(this.cf);
+	g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
     }
 }
