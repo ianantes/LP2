7e33178138b4566dd3e66fed96d928ed0864ca6f
ianantes <49240898+ianantes@users.noreply.github.com>
2021-08-27 20:08:11 -0300

    Create  Oval.java

---
+++ 3.2 exercício/figures/Oval.java
@@ -0,0 +1,33 @@
+package figures;
+
+import java.awt.*;
+
+public class Oval {
+    int x, y;
+    int w, h;
+    private Color cf, cc;
+
+    public Oval (int x, int y, int w, int h,Color cf,Color cc ) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.cf = cf;
+        this.cc = cc;
+        
+        
+    }
+
+    public void print () {
+        System.out.format("Oval de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(this.cc);
+        g2d.drawOval(this.x,this.y, this.w,this.h);
+        g2d.setColor(this.cf);
+        g2d.fillOval(this.x,this.y, this.w,this.h);
+    }
+}
