9973c58fe60a06e02ab0d5220f27be7ded709f49
ianantes <49240898+ianantes@users.noreply.github.com>
2021-09-27 23:18:02 -0300

    Update Figure.java

--- Projeto/Projeto 1/figures/Figure.java
+++ Projeto/Projeto 1/figures/Figure.java
@@ -1,11 +1,25 @@
 package figures;
 
-import java.awt.Graphics;
+import java.awt.*;
+import java.awt.event.*;
+
+
 
 public abstract class Figure {
     int x, y;
     int r, g, b;
     int drawR, drawG, drawB;
-  
+    
+    public void mudarCorContorno(int c1, int c2, int c3){
+	    this.drawR = c1;
+	    this.drawG = c2;
+	    this.drawB = c3;
+    }
+
+    public void mudarCorFundo(int c1, int c2, int c3){
+                this.r = c1;
+		this.g = c2;
+		this.b = c3;
+    }	
     public abstract void paint (Graphics g);
 }
