cded65e820934fad4b7914a0ed55ec37dcce0229
ianantes <49240898+ianantes@users.noreply.github.com>
2021-09-27 23:16:08 -0300

    Update ListApp.java

--- Projeto/Projeto 1/ListApp.java
+++ Projeto/Projeto 1/ListApp.java
@@ -26,36 +26,107 @@ class ListFrame extends JFrame {
             }
         );
 
-        this.addKeyListener (
-            new KeyAdapter() {
-                public void keyPressed (KeyEvent evt) {
-                    int x = rand.nextInt(350);
-                    int y = rand.nextInt(350);
-                    int w = rand.nextInt(50);
-                    int h = rand.nextInt(50);
-		                int ctrlx1 = rand.nextInt(350);
-		                int ctrly1 = rand.nextInt(350);
-		                int x2 = rand.nextInt(350);
-		                int y2 = rand.nextInt(350);
-		                int ctrlx2 = rand.nextInt(350);
-		                int ctrly2 = rand.nextInt(350);
-		                int r = rand.nextInt(256);
-		                int g = rand.nextInt(256);
-		                int b = rand.nextInt(256);
-		                int drawR = rand.nextInt(256);
-		                int drawG = rand.nextInt(256);
-		                int drawB = rand.nextInt(256);
-                    if (evt.getKeyChar() == 'r') {
-                        Rect r1 = new Rect(x,y, w, h, r, g, b, drawR, drawG, drawB);
-                        figs.add(r1);
-                    } else if (evt.getKeyChar() == 'e') {
-                        figs.add(new Ellipse(x,y, w, h, r, g, b, drawR, drawG, drawB));
-                    } else if (evt.getKeyChar() == 'c') {
-			                  figs.add(new CubicCurve(x, y, ctrlx1, ctrly1, x2, y2, ctrlx2, ctrly2, r, g, b, drawR, drawG, drawB));
-		    }
-                    repaint();
-                }
-            }
+         this.addKeyListener (
+                 new KeyAdapter() {
+                                    public void keyPressed (KeyEvent evt) {
+                                        Point pos = new Point(getMousePosition());
+                                        int w = rand.nextInt(50);
+                                        int h = rand.nextInt(50);
+					int r = rand.nextInt(256);
+				        int g = rand.nextInt(256);
+				        int b = rand.nextInt(256);
+				        int drawR = rand.nextInt(256);
+				        int drawG = rand.nextInt(256);
+				        int drawB = rand.nextInt(256);
+					int start = rand.nextInt(350);
+					int extent = rand.nextInt(350);
+					int type = rand.nextInt(3);
+					// Criação das figuras
+					if (evt.getKeyChar() == 'r') {
+					       Rect r1 = new Rect(pos.x, pos.y, w,h, r, g, b, drawR, drawG, drawB);
+					       figs.add(r1);
+				        } else if (evt.getKeyChar() == 'e') {
+						figs.add(new Ellipse(pos.x, pos.y, w,h, r, g, b, drawR, drawG, drawB));
+					} else if (evt.getKeyChar() == 'o') {
+					        figs.add(new Oval(pos.x, pos.y, w, h, r, g, b, drawR, drawG, drawB));
+					} else if (evt.getKeyChar() == 'a') {
+					        figs.add(new Arc(pos.x, pos.y, w, h, start, extent, type, r, g, b, drawR, drawG, drawB));
+					// Deletar a figura que estiver focada
+					} else if(evt.getKeyCode() == KeyEvent.VK_DELETE){
+                                               figs.remove(focus);
+                                               focus = null;
+					       repaint();
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                       // Movimento das figuras através dos teclados UP, DOWN, LEFT, RIGHT
+					} else if(evt.getKeyCode() == KeyEvent.VK_UP){
+				                if(focus != null){
+                                                        focus.mover(0,-1);								   
+					        }								 
+					} else if(evt.getKeyCode() == KeyEvent.VK_DOWN){
+					            if(focus != null){
+                                                         focus.mover(0,1);
+						    }
+					} else if(evt.getKeyCode() == KeyEvent.VK_LEFT){
+					            if(focus != null){
+                                                         focus.mover(-1,0);
+						    }
+					} else if(evt.getKeyCode() == KeyEvent.VK_RIGHT){
+					            if(focus != null){
+                                                         focus.mover(1,0);
+					}
+					// Redimensionamento da figura focada (aumentar e diminuir de tamanho)
+					} else if(evt.getKeyChar() == '+'){
+					        for (Figure fig: figs){
+				                    if(focus == fig){
+                                                       fig.zoom(1,1);							   
+					            }
+				                }
+					
+					} else if(evt.getKeyChar() == '-'){
+					        for (Figure fig: figs){
+				                   if(focus == fig){
+                                                       fig.zoom(-1,-1);								   
+					           }
+				                }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////						
+					// Mudar as cores de fundo
+					} else if(evt.getKeyChar() == '1'){
+					        if(focus!=null){
+							focus.mudarCorFundo(255,0,0);  // vermelho
+						}	
+					} else if(evt.getKeyChar() == '2'){
+					        if(focus!=null){
+							 focus.mudarCorFundo(0,255,0);  // verde
+						}
+                                        } else if(evt.getKeyChar() == '3'){
+					        if(focus!=null){
+							  focus.mudarCorFundo(0,0,255);  // azul
+						}	
+                                        } else if(evt.getKeyChar() == '4'){
+					        if(focus!=null){
+							   focus.mudarCorFundo(255,255,0); // amarelo
+						}	
+					// Mudar as cores de contorno
+                                        } else if(evt.getKeyChar() == '5'){
+					        if(focus!=null){
+							   focus.mudarCorContorno(255,0,0);  // vermelho
+						}	
+					} else if(evt.getKeyChar() == '6'){
+					        if(focus!=null){
+							   focus.mudarCorContorno(0,255,0);  // verde
+						}
+                                        } else if(evt.getKeyChar() == '7'){
+					        if(focus!=null){
+							   focus.mudarCorContorno(0,0,255);  // azul
+						}	
+                                        } else if(evt.getKeyChar() == '8'){
+					        if(focus!=null){
+							   focus.mudarCorContorno(255,255,0);  // amarelo
+					        }	
+                                        }								
+                                        repaint();
+                                  }
+                 }
         );
 
         this.setTitle("Lista de Figuras");
