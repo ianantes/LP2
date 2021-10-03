136a12ab466ec327b3d6f75ddd7ee3510eec1286
Ian <iannantes98@hotmail.com>
2021-10-03 15:31:36 -0300

    Projeto 1 finalizado

--- Projeto/Projeto 1/ListApp.java
+++ Projeto/Projeto 1/ListApp.java
@@ -15,6 +15,7 @@ class ListApp {
 
 class ListFrame extends JFrame {
     ArrayList<Figure> figs = new ArrayList<Figure>();
+	Figure focus = null;
     Random rand = new Random();
 
     ListFrame () {
@@ -25,141 +26,134 @@ class ListFrame extends JFrame {
                 }
             }
         );
-
-	this.addMouseListener (
-		  new MouseAdapter() {
+		
+		this.addMouseListener (
+		    new MouseAdapter() {
 			    public void mousePressed (MouseEvent evt) { 
 				   Point pos = new Point(getMousePosition());
-                                   focus = null; 
+                   focus = null; 				
 				   for (Figure fig: figs){
-				        if(fig.clicked(pos.x, pos.y))     
-                                        {
-                                            focus = fig;
-					    figs.remove(fig);
-					    figs.add(fig);
-					    repaint();
-					    break;
-                                        }
-					else
-					{
-					    focus = null;
-					    repaint();
-					}					
+				        if((pos.x >= fig.x && pos.x <= (fig.w+fig.x)) && (pos.y >= fig.y && pos.y <= (fig.y+fig.h))) {
+							focus = fig;
+							figs.remove(fig);
+							figs.add(fig);
+							repaint();
+							break;
+                        }
+                        else
+                        {
+                            focus = null;
+                            repaint();
+                        }					
 				    }
-					    
-	 );
-				    
-         this.addKeyListener (
-                 new KeyAdapter() {
-                                    public void keyPressed (KeyEvent evt) {
-                                        Point pos = new Point(getMousePosition());
-                                        int w = rand.nextInt(50);
-                                        int h = rand.nextInt(50);
+				}
+			}
+		);
+		
+		this.addMouseMotionListener (
+		    new MouseMotionAdapter() {
+			    public void mouseDragged (MouseEvent evt) {         
+				    Point pos = new Point(getMousePosition()); 
+                    if (focus != null){
+                       focus.move(pos.x-focus.x, pos.y-focus.y);
+					   repaint();
+					}					
+				}
+			}
+		);
+		
+
+        this.addKeyListener (
+            new KeyAdapter() {
+                public void keyPressed (KeyEvent evt) {
+                    Point pos = new Point(getMousePosition());
+                    int w = rand.nextInt(50);
+                    int h = rand.nextInt(50);
 					int r = rand.nextInt(256);
-				        int g = rand.nextInt(256);
-				        int b = rand.nextInt(256);
-				        int drawR = rand.nextInt(256);
-				        int drawG = rand.nextInt(256);
-				        int drawB = rand.nextInt(256);
+				    int g = rand.nextInt(256);
+				    int b = rand.nextInt(256);
+				    int drawR = rand.nextInt(256);
+				    int drawG = rand.nextInt(256);
+				    int drawB = rand.nextInt(256);
 					int start = rand.nextInt(350);
 					int extent = rand.nextInt(350);
 					int type = rand.nextInt(3);
 					// Criação das figuras
-					if (evt.getKeyChar() == 'r') {
-					       Rect r1 = new Rect(pos.x, pos.y, w,h, r, g, b, drawR, drawG, drawB);
-					       figs.add(r1);
-				        } else if (evt.getKeyChar() == 'e') {
-						figs.add(new Ellipse(pos.x, pos.y, w,h, r, g, b, drawR, drawG, drawB));
-					} else if (evt.getKeyChar() == 'o') {
-					        figs.add(new Oval(pos.x, pos.y, w, h, r, g, b, drawR, drawG, drawB));
+                    if (evt.getKeyChar() == 'r') {
+                        figs.add(new Rect(pos.x, pos.y, w,h, r, g, b, drawR, drawG, drawB));
+                    } else if (evt.getKeyChar() == 'e') {
+                        figs.add(new Ellipse(pos.x, pos.y, w,h, r, g, b, drawR, drawG, drawB));
+                    } else if (evt.getKeyChar() == 'o') {
+					    figs.add(new Oval(pos.x, pos.y, w, h, r, g, b, drawR, drawG, drawB));
 					} else if (evt.getKeyChar() == 'a') {
-					        figs.add(new Arc(pos.x, pos.y, w, h, start, extent, type, r, g, b, drawR, drawG, drawB));
-					// Deletar a figura que estiver focada
+					    figs.add(new Arc(pos.x, pos.y, w, h, start, extent, type, r, g, b, drawR, drawG, drawB));
+					// Deletar a figura focada
 					} else if(evt.getKeyCode() == KeyEvent.VK_DELETE){
-                                               figs.remove(focus);
-                                               focus = null;
-					       repaint();
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                                       // Movimento das figuras através dos teclados UP, DOWN, LEFT, RIGHT
+                            figs.remove(focus);
+                            focus = null;
+							repaint();
+                    // Movimento das figuras através dos teclados UP, DOWN, LEFT, RIGHT
 					} else if(evt.getKeyCode() == KeyEvent.VK_UP){
 				                if(focus != null){
-                                                        focus.mover(0,-1);								   
-					        }								 
+                                   focus.move(0,-1);								   
+					            }								 
 					} else if(evt.getKeyCode() == KeyEvent.VK_DOWN){
 					            if(focus != null){
-                                                         focus.mover(0,1);
-						    }
+                                   focus.move(0,1);
+								}
 					} else if(evt.getKeyCode() == KeyEvent.VK_LEFT){
 					            if(focus != null){
-                                                         focus.mover(-1,0);
-						    }
+                                   focus.move(-1,0);
+								}
 					} else if(evt.getKeyCode() == KeyEvent.VK_RIGHT){
 					            if(focus != null){
-                                                         focus.mover(1,0);
-					}
-					// Redimensionamento da figura focada (aumentar e diminuir de tamanho)
+                                   focus.move(1,0);
+								}
+					// Aumentar e diminuir de tamanho a figura focada
 					} else if(evt.getKeyChar() == '+'){
 					        for (Figure fig: figs){
-				                    if(focus == fig){
-                                                       fig.zoom(1,1);							   
+				                if(focus == fig){
+                                   fig.zoom(1,1);							   
 					            }
-				                }
+				            }
 					
 					} else if(evt.getKeyChar() == '-'){
 					        for (Figure fig: figs){
-				                   if(focus == fig){
-                                                       fig.zoom(-1,-1);								   
-					           }
-				                }
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////						
-					// Mudar as cores de fundo
-					} else if(evt.getKeyChar() == '1'){
+				                if(focus == fig){
+                                   fig.zoom(-1,-1);								   
+					            }
+				            }
+					// Mudar a cor de fundo
+                    } else if(evt.getKeyChar() == 'f'){
 					        if(focus!=null){
-							focus.mudarCorFundo(255,0,0);  // vermelho
-						}	
-					} else if(evt.getKeyChar() == '2'){
+							   focus.cf(r,g,b); // cor aleatória
+							}	
+					// Mudar a cor de contorno
+                    } else if(evt.getKeyChar() == 'c'){
 					        if(focus!=null){
-							 focus.mudarCorFundo(0,255,0);  // verde
-						}
-                                        } else if(evt.getKeyChar() == '3'){
-					        if(focus!=null){
-							  focus.mudarCorFundo(0,0,255);  // azul
-						}	
-                                        } else if(evt.getKeyChar() == '4'){
-					        if(focus!=null){
-							   focus.mudarCorFundo(255,255,0); // amarelo
-						}	
-					// Mudar as cores de contorno
-                                        } else if(evt.getKeyChar() == '5'){
-					        if(focus!=null){
-							   focus.mudarCorContorno(255,0,0);  // vermelho
-						}	
-					} else if(evt.getKeyChar() == '6'){
-					        if(focus!=null){
-							   focus.mudarCorContorno(0,255,0);  // verde
-						}
-                                        } else if(evt.getKeyChar() == '7'){
-					        if(focus!=null){
-							   focus.mudarCorContorno(0,0,255);  // azul
-						}	
-                                        } else if(evt.getKeyChar() == '8'){
-					        if(focus!=null){
-							   focus.mudarCorContorno(255,255,0);  // amarelo
-					        }	
-                                        }								
-                                        repaint();
-                                  }
-                 }
+							   focus.cc(drawR,drawG,drawB);  // cor aleatória
+							}	
+                    }								
+                    repaint();
+                }
+            }
         );
 
         this.setTitle("Lista de Figuras");
-        this.setSize(350, 350);
+        this.setSize(500, 500);
     }
 
     public void paint (Graphics g) {
         super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
         for (Figure fig: this.figs) {
             fig.paint(g);
         }
+		
+		if(focus != null){
+			g2d.setColor(Color.red);
+		    g2d.drawRect(focus.x-4, focus.y-4, focus.w+8, focus.h+8);
+		    focus.paint(g);
+		}
     }
 }
--- Projeto/Projeto 1/figures/Figure.java
+++ Projeto/Projeto 1/figures/Figure.java
@@ -6,20 +6,32 @@ import java.awt.event.*;
 
 
 public abstract class Figure {
-    int x, y;
-    int r, g, b;
-    int drawR, drawG, drawB;
+    public int x, y, w, h;
+    public int r, g, b;
+    public int drawR, drawG, drawB;
     
-    public void mudarCorContorno(int c1, int c2, int c3){
+    public void cc(int c1, int c2, int c3){
 	    this.drawR = c1;
 	    this.drawG = c2;
 	    this.drawB = c3;
     }
 
-    public void mudarCorFundo(int c1, int c2, int c3){
-                this.r = c1;
+    public void cf(int c1, int c2, int c3){
+        this.r = c1;
 		this.g = c2;
 		this.b = c3;
+    }
+
+     public void zoom (int dw, int dh){
+	    this.w += dw;
+	    this.h += dh;
+		
+    }
+	
+    public void move (int dx, int dy){
+	    this.x += dx;
+	    this.y += dy;
+		
     }	
     public abstract void paint (Graphics g);
 }
