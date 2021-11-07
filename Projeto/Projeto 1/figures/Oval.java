3b2da64ef2d6d653d9e362c00f8f8ea7cd3069d0
ianantes <49240898+ianantes@users.noreply.github.com>
2021-11-07 20:54:14 -0300

    Update Oval.java

--- Projeto/Projeto 1/figures/Oval.java
+++ Projeto/Projeto 1/figures/Oval.java
@@ -18,7 +18,7 @@ public class Oval extends Figure {
 		this.drawB = drawB;
 	}
 	
-	public void print() {
+	private void print() {
 	    System.out.format("Oval de tamanho (%d,%d) na posicao (%d,%d).\n", this.w, this.h, this.x, this.y);
 	}
 	
