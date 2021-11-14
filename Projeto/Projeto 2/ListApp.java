d36f941f0905f0049a985fcb6f76dd92fdee52bb
ianantes <49240898+ianantes@users.noreply.github.com>
2021-11-14 14:09:21 -0300

    Update and rename Projeto/Projeto 1/ListApp.java to Projeto/Projeto 2/ListApp.java

--- Projeto/Projeto 2/ListApp.java
+++ Projeto/Projeto 2/ListApp.java
@@ -3,6 +3,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Random;
+import java.io.*;
 
 import figures.*;
 
@@ -15,23 +16,53 @@ class ListApp {
 
 class ListFrame extends JFrame {
     ArrayList<Figure> figs = new ArrayList<Figure>();
+	ArrayList<Button> buts = new ArrayList<Button>();
 	Figure focus = null;
+	Button but_focus = null;
+	int ind;
     Random rand = new Random();
 
     ListFrame () {
+		// Leitura do arquivo proj.bin
+		try {   
+		     FileInputStream f = new FileInputStream("proj.bin");
+		     ObjectInputStream o = new ObjectInputStream(f);
+		     this.figs = (ArrayList<Figure>) o.readObject();
+		     o.close();
+	    } catch(Exception x) {
+		     System.out.println("ERRO!");
+	    }
+		
         this.addWindowListener (
             new WindowAdapter() {
                 public void windowClosing (WindowEvent e) {
+					// Escrita no arquivo proj.bin
+					try {    
+					     FileOutputStream f = new FileOutputStream("proj.bin");
+					     ObjectOutputStream o = new ObjectOutputStream(f);
+					     o.writeObject(figs);
+					     o.flush();
+					     o.close();
+				    } catch (Exception x) {
+				    }	
                     System.exit(0);
                 }
             }
         );
 		
+		setFocusTraversalKeysEnabled(false);
+		
+	    buts.add(new Button(1, new Rect(0,0,0,0,0,0,0,0,0,0)));
+	    buts.add(new Button(2, new Ellipse(0,0,0,0,0,0,0,0,0,0)));
+	    buts.add(new Button(3, new Arc(0,0,0,0,180,250,0,0,0,0,0,0,0)));
+	    buts.add(new Button(4, new Oval(0,0,0,0,0,0,0,0,0,0)));
+		
 		this.addMouseListener (
 		    new MouseAdapter() {
 			    public void mousePressed (MouseEvent evt) { 
 				   Point pos = new Point(getMousePosition());
-                   focus = null; 				
+                   focus = null; 
+                   but_focus = null;				   
 				   for (Figure fig: figs){
 				        if(fig.clicked(pos.x, pos.y)) {
 							focus = fig;
@@ -45,6 +76,51 @@ class ListFrame extends JFrame {
                             focus = null;
                             repaint();
                         }					
+				    }
+					
+					for (Button but: buts) {
+		                if (but.clicked(pos.x,pos.y)) {
+			                but_focus = but;
+			                repaint();
+			                break;
+			            }
+		            }
+					
+					// Quando um dos botões ganha foco, é guardado o índice para saber qual a figura referente ao botão clicado
+				    if (but_focus != null) {
+						if (but_focus.idx == 1) {
+							ind = 1;
+						} 
+						else if (but_focus.idx == 2) {
+						    ind = 2;
+						}
+						else if (but_focus.idx == 3) {
+							ind = 3;
+						}
+						else if (but_focus.idx == 4) {
+							ind = 4;
+						}
+						repaint();
+				     }
+				    // Quando está sem foco o botão e a figura, é feito, clicando no mouse, a figura referente ao último botão clicado
+				    if ((but_focus == null) && (focus == null)) {
+						if (ind == 1) {
+							figs.add(new Rect(pos.x,pos.y, rand.nextInt(50),rand.nextInt(50),0,0,0,0,0,0));
+							ind = 0;
+						} 
+						else if (ind == 2) {
+							figs.add(new Ellipse(pos.x,pos.y, rand.nextInt(50),rand.nextInt(50),0,0,0,0,0,0));
+							ind = 0;
+						}
+						else if (ind == 3) {
+							figs.add(new Arc(pos.x,pos.y, rand.nextInt(50),rand.nextInt(50), rand.nextInt(280),rand.nextInt(280), rand.nextInt(3),0,0,0,0,0,0));
+							ind = 0;
+						}
+						else if (ind == 4) {
+							figs.add(new Oval(pos.x,pos.y, rand.nextInt(50),rand.nextInt(50),0,0,0,0,0,0));
+							ind = 0;
+						}
+						repaint();
 				    }
 				}
 			}
@@ -132,8 +208,18 @@ class ListFrame extends JFrame {
                     } else if(evt.getKeyChar() == 'c'){
 					        if(focus!=null){
 							   focus.cc(drawR,drawG,drawB);  // cor aleatória
-							}	
-                    }								
+							}
+                    // Mudança de foco da figura							
+                    } else if(evt.getKeyCode() == KeyEvent.VK_TAB){
+				            for (Figure fig: figs) {
+					           if (focus != fig) {
+					              focus = fig;
+					              figs.remove(fig);
+					              figs.add(fig);
+					              break;
+					            }
+				            }    
+			        }										
                     repaint();
                 }
             }
@@ -147,13 +233,15 @@ class ListFrame extends JFrame {
         super.paint(g);
 		Graphics2D g2d = (Graphics2D) g;
         for (Figure fig: this.figs) {
-            fig.paint(g);
+            fig.paint(g, false);
         }
 		
 		if(focus != null){
-			g2d.setColor(Color.red);
-		    g2d.drawRect(focus.x-4, focus.y-4, focus.w+8, focus.h+8);
-		    focus.paint(g);
+		    focus.paint(g, true);
 		}
+		
+		for(Button but: this.buts) {
+	        but.paint(g, but == but_focus);
+	    }
     }
 }
