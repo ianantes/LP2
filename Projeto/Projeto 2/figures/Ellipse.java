1df572b8e3228373480a44b0df8aa7b1d79a4822
ianantes <49240898+ianantes@users.noreply.github.com>
2021-11-14 13:57:53 -0300

    Update Ellipse.java

--- Projeto/Projeto 2/figures/Ellipse.java
+++ Projeto/Projeto 2/figures/Ellipse.java
@@ -6,9 +6,9 @@ import java.awt.geom.Ellipse2D.Double;
 
 public class Ellipse extends Figure {
 	
-        public Ellipse (int x, int y, int w, int h, int r, int g, int b, int drawR, int drawG, int drawB) {
+                public Ellipse (int x, int y, int w, int h, int r, int g, int b, int drawR, int drawG, int drawB) {
 			super(x, y, w, h, r, g, b, drawR, drawG, drawB);
-	    }
+	        }
 	
 		private void print() {
 			System.out.format("Elipse de tamanho (%d,%d) na posicao (%d,%d).\n", this.w, this.h, this.x, this.y);
@@ -18,9 +18,9 @@ public class Ellipse extends Figure {
 			Graphics2D g2d = (Graphics2D) g;
 			
 			if(focused) {
-		      g2d.setColor(Color.red);
-		      g2d.draw(new Ellipse2D.Double(this.x-2, this.y-2, this.w+4, this.h+4));
-		    }
+		            g2d.setColor(Color.red);
+		            g2d.draw(new Ellipse2D.Double(this.x-2, this.y-2, this.w+4, this.h+4));
+		        }
 			
 			g2d.setColor(new Color(this.r, this.g, this.b));
 			g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
